ac52143151ee1d861a1ba476659dbd6f59545abf
Tamimul Islam <hmtamim24@gmail.com>
2022-11-08 22:48:30 +0600

    react photo view feature added

--- src/shared/ServiceCard/ServiceCard.js
+++ src/shared/ServiceCard/ServiceCard.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
   const { _id, name, image, price, description } = service;
   return (
-    <div className="p-3 border shadow-2xl rounded-md relative" key={_id}>
-      <img className="w-full h-64 rounded-t-md" src={image} alt={name} />
+    <PhotoProvider>
+        <div className="p-3 border shadow-2xl rounded-md relative">
+      <PhotoView src={image}>
+      <Link title="click to see full screen"><img className="w-full h-64 rounded-t-md" src={image} alt={name} /></Link>
+      </PhotoView>
 
       <h2 className="text-2xl font-bold mt-2">{name}</h2>
       <p className="my-2 mb-8">{description.slice(0, 100)}...</p>
@@ -15,6 +21,7 @@ const ServiceCard = ({ service }) => {
         </div>
       </div>
     </div>
+    </PhotoProvider>
   );
 };
 
