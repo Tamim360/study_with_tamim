bd473e061638bea31085840bc1d717c3c3df33b6
Tamimul Islam <hmtamim24@gmail.com>
2022-11-10 15:09:40 +0600

    favicon changed

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,9 +1,6 @@
 import { useEffect } from "react"
-import { useLocation } from "react-router-dom"
 
 const useTitle = (title) => {
-    const location = useLocation()
-    const pathname = location.pathname
     useEffect(() => {
         document.title = `${title} - Study With Tamim`
     },[title])
