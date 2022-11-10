f2c6d4df4e414d87d1ad4f51d5bc3a674339128f
Tamimul Islam <hmtamim24@gmail.com>
2022-11-10 10:18:59 +0600

    subscribe section added to home page

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Faq from '../Faq/Faq';
 import Services from '../Services/Services';
+import Subscribe from '../Subscribe/Subscribe';
 import WhyChoose from '../WhyChoose/WhyChoose';
 
 const Home = () => {
@@ -10,7 +11,8 @@ const Home = () => {
             <Banner/>
             <Services />
             <WhyChoose />
-            <Faq/>
+            <Faq />
+            <Subscribe/>
         </div>
     );
 };
